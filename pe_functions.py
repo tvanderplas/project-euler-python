38d6f3aade9b8a9b4e79e7b291c2f91eced8339d
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2019-03-09 13:04:47 -0800

    create file with all reusable functions

---
+++ pe_functions.py
@@ -0,0 +1,9 @@
+from pe_02 import fibonacci_generator
+from pe_03 import get_primes, get_prime_factors
+from pe_14 import collatz
+from pe_15 import catalan
+from pe_17 import count_letters
+from pe_18 import max_path
+from pe_19 import days_in_month, is_leap_year
+from pe_20 import factorial
+from pe_21 import divisors
