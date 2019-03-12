f76141969f447cf8243f6a4b06c17ac1d834af73
tvanderplas <rofldong@null.net>
2019-03-12 16:10:19 -0400

    add more tests, finish solution

--- pe_24.py
+++ pe_24.py
@@ -8,12 +8,25 @@
 
 from pe_functions import factorial
 
-def permutations(word: str, answer: list = []):
+def permutations(word: str):
 	if len(word) < 2:
 		return [word]
 	base = word[:2]
+	answer = []
 	answer.extend([base, base[::-1]])
-	while False: #len(answer[0]) < len(word):
-		for char in base:
-			pass
+	next_answer = []
+	for x in range(2, len(word)):
+		for item in answer:
+			for n in range(len(item) + 1):
+				an_item = list(item)
+				an_item.insert(n, word[x])
+				next_answer.append(''.join([x for x in an_item]))
+		answer.clear()
+		answer.extend(next_answer)
+		next_answer.clear()
+		answer.sort()
 	return answer
+
+if __name__ == '__main__':
+	answer = permutations('0123456789')
+	print(answer[999999])
--- pe_24_test.py
+++ pe_24_test.py
@@ -1,17 +1,25 @@
 from pe_24 import permutations
+from pe_functions import factorial
 import unittest
 
 class permutations_test(unittest.TestCase):
 
 	def test_permutations_length_1(self):
 		self.assertEqual(permutations('0'), ['0'])
+		self.assertEqual(len(permutations('0')), factorial(1))
 	def test_permutations_length_2(self):
 		self.assertEqual(permutations('01'), ['01','10'])
+		self.assertEqual(len(permutations('01')), factorial(2))
 	def test_permutations_length_3(self):
 		self.assertEqual(permutations('012'), ['012', '021', '102', '120', '201', '210'])
-
-
-
+		self.assertEqual(len([int(x) for x in permutations('012')]), factorial(3))
+	def test_permutations_length_4(self):
+		self.assertEqual(permutations('ABCD'), 
+			['ABCD', 'ABDC', 'ACBD', 'ACDB', 'ADBC', 'ADCB',
+			'BACD', 'BADC', 'BCAD', 'BCDA', 'BDAC', 'BDCA',
+			'CABD', 'CADB', 'CBAD', 'CBDA', 'CDAB', 'CDBA',
+			'DABC', 'DACB', 'DBAC', 'DBCA', 'DCAB', 'DCBA'])
+		self.assertEqual(len([int(x) for x in permutations('0123')]), factorial(4))
 
 if __name__ == '__main__':
 	unittest.main()
